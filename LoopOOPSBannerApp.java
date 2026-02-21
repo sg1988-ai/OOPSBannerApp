af7074f2a61dd9de24a8123defe7fe70d7649be7
sg1988-ai <sg1988@srmist.edu.in>
2026-02-21 10:08:19 +0530

    Add files via upload

---
+++ LoopOOPSBannerApp.java
@@ -0,0 +1,16 @@
+public class LoopOOPSBannerApp  {
+
+    public static void main(String[] args) {
+		String[] lines = new String[10];
+
+        lines[0] = String.join(" ","  ***   ","    ***   ","  ******  ","  ***** ");System.out.println(lines[0]);
+        lines[1] = String.join(" "," **  ** ","  **   ** ","  **   ** "," **    **");System.out.println(lines[1]);
+        lines[2] = String.join(" ","**    **"," **     **","  **   ** "," **");System.out.println(lines[2]);
+        lines[3] = String.join(" ","**    **"," **     **","  **   ** "," **");System.out.println(lines[3]);
+        lines[4] = String.join(" ","**    **"," **     **","  ******  ","   ***");System.out.println(lines[4]);
+        lines[5] = String.join(" ","**    **"," **     **","  **      ","       **");System.out.println(lines[5]);
+        lines[6] = String.join(" ","**    **"," **     **","  **      ","       **");System.out.println(lines[6]);
+        lines[7] = String.join(" "," **  ** ","  **   ** ","  **      ","       **");System.out.println(lines[7]);
+        lines[8] = String.join(" ","  ***   ","    ***   ","  **      ","  ******");System.out.println(lines[8]);
+    }
+}
