741062038307e0d058a87ced4091fb0993212b97
sg1988-ai <sg1988@srmist.edu.in>
2026-03-02 13:04:09 +0530

    Add files via upload

---
+++ BannerApp.java
@@ -0,0 +1,57 @@
+public class BannerApp {
+
+    public static String[] getOPattern() {
+        return new String[]{
+                "  *****  ",
+                " *     * ",
+                "*       *",
+                "*       *",
+                "*       *",
+                " *     * ",
+                "  *****  "
+        };
+    }
+
+    public static String[] getPPattern() {
+        return new String[]{
+                " ******  ",
+                " *     * ",
+                " *     * ",
+                " ******  ",
+                " *       ",
+                " *       ",
+                " *       "
+        };
+    }
+
+    public static String[] getSPattern() {
+        return new String[]{
+                "  *****  ",
+                " *     * ",
+                " *       ",
+                "  *****  ",
+                "       * ",
+                " *     * ",
+                "  *****  "
+        };
+    }
+
+    public static void main(String[] args) {
+
+        String[] o1 = getOPattern();
+        String[] o2 = getOPattern();
+        String[] p = getPPattern();
+        String[] s = getSPattern();
+
+        String[] banner = new String[7];
+
+        for (int i = 0; i < 7; i++) {
+            banner[i] = String.join("  ", o1[i], o2[i], p[i], s[i]);
+        }
+
+        // Enhanced for loop to print
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
